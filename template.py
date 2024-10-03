eda37d161b043173e8329478fb69f22f02e32ca0
srabani-tripathy <srabanitripathy04@gmail.com>
2024-10-03 17:50:34 +0530

    project structure added

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+
+project_name = "UAQHI"
+
+list_of_files = [
+
+    f"{project_name}/_init_.py",
+    f"{project_name}/components/_init_.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/_init_.py",
+    f"{project_name}/constants/_init_.py",
+    f"{project_name}/entity/_init_.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/_init_.py",
+    f"{project_name}/logger/_init_.py",
+    f"{project_name}/pipline/_init_.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/utils/_init_.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "config/model.yaml",
+    "config/schema.yaml",
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at: {filepath}")
